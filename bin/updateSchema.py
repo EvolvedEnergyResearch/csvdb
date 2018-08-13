801f5c41beca20733fff328bb9526e432a5265ac
Richard Plevin <rich@plevin.com>
2018-08-13 16:58:49 -0700

    Added support to updateSchema.py for arbitrary number of subdirectories in a CSV database

--- bin/updateSchema.py
+++ bin/updateSchema.py
@@ -2,12 +2,125 @@
 from __future__ import print_function
 
 import click
-from glob import glob
+import gzip
 import os
 import pandas as pd
+import re
 import shutil
 
+from csvdb.database import SHAPE_DIR
+from csvdb.error import CsvdbException
+
+Tables_to_skip = ['GEOGRAPHIES_SPATIAL_JOIN']
+
 DefaultSchemaFile = 'csvdb-schema.csv'
+
+# case-insensitive match of *.csv and *.csv.gz files
+CsvPattern = re.compile('.*\.csv(\.gz)?$', re.IGNORECASE)
+
+# case-insensitive match of *.gz files
+ZipPattern = re.compile('.*\.gz$', re.IGNORECASE)
+
+
+def create_file_map(dbdir):
+    file_map = {}  # maps table names => file names under the database root folder
+
+    if not os.path.exists(dbdir):
+        raise CsvdbException('Database path "{}" does not exist'.format(dbdir))
+
+    if not os.path.isdir(dbdir):
+        raise CsvdbException('Database path "{}" is not a directory'.format(dbdir))
+
+    prefixLen = len(dbdir) + 1
+
+    for dirpath, dirnames, filenames in os.walk(dbdir, topdown=False):
+        if os.path.basename(dirpath) == SHAPE_DIR:
+            continue
+
+        for filename in filenames:
+            if re.match(CsvPattern, filename):
+                basename = os.path.basename(filename)
+                tblname = basename.split('.')[0]   # removes either .csv or .csv.gz
+                abspath = os.path.abspath(os.path.join(dirpath, filename))
+                file_map[tblname] = abspath[prefixLen:]     # save path relative to dbdir
+
+    print("Found {} .CSV files for {}".format(len(file_map), dbdir))
+    return file_map
+
+def create_schema_file(dbdir, schema_file):
+    file_map = create_file_map(dbdir)
+
+    with open(schema_file, 'w') as schema:
+        for tblname, csvFile in file_map.items():
+
+            if not tblname in Tables_to_skip:
+                openFunc = gzip.open if re.match(ZipPattern, csvFile) else open
+                abspath = os.path.join(dbdir, csvFile)
+
+                with openFunc(abspath, 'rb') as csv:
+                    header = csv.readline()
+                    schema.write(csvFile + ',')  # insert CSV basename in first column
+                    schema.write(header)
+
+def update_from_schema(dbdir, schema_file, run, verbose):
+    file_map = create_file_map(dbdir)
+
+    with open(schema_file, 'r') as schema:
+        lines = schema.readlines()
+
+    for line in lines:
+        line = line.strip()
+        source_cols = line.split(',')
+        relpath = source_cols.pop(0)
+        abspath = os.path.join(dbdir, relpath)
+
+        basename = os.path.basename(relpath)
+        tblname = basename.split('.')[0]
+
+        csvFile = file_map.get(tblname)
+
+        if not (csvFile and os.path.exists(abspath)):
+            print('Creating empty file {}'.format(relpath))
+            if run:
+                with open(abspath, 'w') as f:
+                    f.write(','.join(source_cols))
+                    f.write('\n')
+
+            continue
+
+        with open(abspath, 'r') as csv:
+            header = csv.readline().strip()
+
+        target_cols = header.split(',')
+
+        if target_cols == source_cols:
+            if verbose:
+                print('{}: OK'.format(relpath))
+
+        else:
+            source_set = set(map(str.strip, source_cols))
+            target_set = set(map(str.strip, target_cols))
+
+            extra = target_set - source_set
+            missing = source_set - target_set
+            reorder = (source_set == target_set)  # we already know source_cols != target_cols
+
+            print('{}:'.format(relpath))
+            extra   and print(' - dropping extra columns {}'.format(sorted(extra)))
+            missing and print(' - adding missing cols {}'.format(sorted(missing)))
+            reorder and print(' - resetting column order')
+
+            if run:
+                shutil.copy2(abspath, abspath + '~')  # create backup file
+
+                new = pd.DataFrame(columns=source_cols)  # all columns, in correct order
+                old = pd.read_csv(abspath, index_col=None)
+                old.columns = map(str.strip, old.columns)   # strip whitespace
+
+                for col in source_set.intersection(target_set):
+                    new[col] = old[col]
+
+                new.to_csv(abspath, index=None)
 
 @click.command()
 
@@ -30,67 +143,12 @@ DefaultSchemaFile = 'csvdb-schema.csv'
 
 
 def main(dbdir, schema_file, mode, run, verbose):
-    csvFiles = glob(os.path.join(dbdir, '*.csv'))
-
     if mode == 'save':
-        with open(schema_file, 'w') as schema:
-            for csvFile in csvFiles:
-                basename = os.path.basename(csvFile)
-                with open(csvFile, 'r') as csv:
-                    header = csv.readline()
-                    schema.write(basename + ',')        # insert CSV basename in first column
-                    schema.write(header)
+        create_schema_file(dbdir, schema_file)
 
     else: # mode is 'update'
-        with open(schema_file, 'r') as schema:
-            lines = schema.readlines()
+        update_from_schema(dbdir, schema_file, run, verbose)
 
-        for line in lines:
-            line = line.strip()
-            source_cols = line.split(',')
-            basename = source_cols.pop(0)
-
-            csvFile = os.path.join(dbdir, basename)
-
-            if not os.path.exists(csvFile):
-                print('Creating empty file {}'.format(basename))
-                if run:
-                    with open(csvFile, 'w') as f:
-                        f.write(','.join(source_cols))
-                        f.write('\n')
-
-                continue
-
-            with open(csvFile, 'r') as csv:
-                header = csv.readline().strip()
-
-            target_cols = header.split(',')
-            if target_cols == source_cols:
-                if verbose:
-                    print('{}: OK'.format(basename))
-
-            else:
-                source_set = set(source_cols)
-                target_set = set(target_cols)
-                extra   = target_set - source_set
-                missing = source_set - target_set
-                reorder = (source_set == target_set) # we already know source_cols != target_cols
-
-                print('{}:'.format(basename))
-                extra   and print(' - dropping extra columns {}'.format(sorted(extra)))
-                missing and print(' - adding missing cols {}'.format(sorted(missing)))
-                reorder and print(' - resetting column order')
-
-                if run:
-                    shutil.copy2(csvFile, csvFile + '~')        # create backup file
-
-                    new = pd.DataFrame(columns=source_cols)     # all columns, in correct order
-                    old = pd.read_csv(csvFile, index_col=None)
-
-                    for col in source_set.intersection(target_set):
-                        new[col] = old[col]
-
-                    new.to_csv(csvFile, index=None)
 
 if __name__ == '__main__':
     main()
