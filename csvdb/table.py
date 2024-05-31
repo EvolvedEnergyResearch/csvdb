865067996663bc06dea3d1ebdd5d5a9513c262ef
Ryan Jones <ryandrewjones@gmail.com>
2024-05-30 20:20:53 -0600

    specifying encoding

--- csvdb/table.py
+++ csvdb/table.py
@@ -119,7 +119,7 @@ class CsvTable(object):
             wait = 1
             while True:
                 try:
-                    with openFunc(fn, 'r') as f:
+                    with openFunc(fn, 'r', encoding=None if fn.endswith('.gz') else 'utf-8') as f:
                         dfs.append(pd.read_csv(f, index_col=None, converters=converters, na_values='', low_memory=False))
                     break
                 except (OSError, pd.errors.EmptyDataError) as e:
