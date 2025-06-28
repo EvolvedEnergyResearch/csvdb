09813d2e5d22119151ee5211b14ef22046cc7efd
Ryan Jones <ryandrewjones@gmail.com>
2025-06-27 20:00:20 -0600

    updating setup script

--- setup.py
+++ setup.py
@@ -1,20 +1,36 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='csvdb',
-    version=1.0,
+    version=2.0,
     author='Rich Plevin and Ryan Jones',
-    description='Python 3.8 library and scripts for interacting with data stored in CSV files.',
-    platforms=['Windows', 'MacOS', 'Linux'],
-
+    author_email="ryan.jones@evolved.energy",
+    description="Python library and scripts for interacting with data stored in CSV files.",
+    long_description=long_description,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",  # Adjust as needed
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.8",
     packages=['csvdb'],
     entry_points={'console_scripts': ['genClasses = csvdb.genClasses:main',
                                       'csvdbSchema = csvdb.validation:main']},
-    install_requires=['pandas'],
-    include_package_data = False,
-
+    install_requires=[
+        "pandas",
+        "polars",
+    ],
     url='https://github.com/EvolvedEnergyResearch/csvdb',
     download_url='https://github.com/EvolvedEnergyResearch/csvdb.git',
-
-    zip_safe=True,
+    include_package_data = False,
+    zip_safe=False,
 )
